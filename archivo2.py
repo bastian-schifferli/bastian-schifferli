c1b37829f54bed77486c8c7aa7402fcfd916bcf4
bastian-schifferli <101894222+bastian-schifferli@users.noreply.github.com>
2022-03-21 17:23:10 -0300

    archivo

---
+++ archivo2.py
@@ -0,0 +1,9 @@
+nom = input("ingrese su nombre")
+ape = input)("ingrese su apellido")
+edad = input("ingrese su edad")
+num1 = input("ingrese numero 1")
+num2 = input("ingrese numero 2")
+suma = num1 + num2
+print(f"hola: {nom}")
+print(f"apellido: {ape}")
+print(f"edad: {edad}")
